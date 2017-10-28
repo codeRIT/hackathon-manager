f36db3ed90bca4b064e33a76beda6fe320fca195
Stuart Olivera <stuart@stuartolivera.com>
2017-10-28 16:18:09 -0400

    Fix us.json for dashboard map (#42)

--- lib/hackathon_manager/engine.rb
+++ lib/hackathon_manager/engine.rb
@@ -11,8 +11,12 @@ module HackathonManager
         hackathon_manager/vendor/*.js
         hackathon_manager/vendor/*.css
         hackathon_manager/mailer.css
-        hackathon_manager/us.json
       ]
+    end
+
+    # Initializer to combine this engines static assets with the static assets of the hosting site.
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
     end
 
     ActionController::Base.class_eval do
