a7a40a0eacf21b20f45193e19f6caf269a4baf9a
Stuart Olivera <stuart@stuartolivera.com>
2017-12-04 14:10:24 -0500

    Split javascript into separate files

---
+++ app/assets/javascripts/hackathon_manager/toggle.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $.fn.toggleDetails = function() {
+    var toggle = function() {
+      $('.details').slideUp();
+      $(this).parent().find('.details:hidden').slideDown();
+    };
+
+    $(this).each(function() {
+      $(this).on('click', toggle);
+    });
+  };
+
+  $('.toggle-details').toggleDetails();
+
+  $.fn.toggleBlock = function() {
+    var toggle = function() {
+      var $this = $(this);
+      $('.info').slideUp();
+      $('#' + $this.data('target') + ':hidden').slideDown();
+    };
+    $(this).each(function() {
+      $(this).on('click', toggle);
+    });
+  };
+  $('.block .name').toggleBlock();
+});
