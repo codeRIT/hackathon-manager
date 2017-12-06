2cdacb8644c1a5845d648af9a51a7535d86837eb
Stuart Olivera <stuart@stuartolivera.com>
2017-12-06 16:27:37 -0500

    Reload config upon hackathon.yml changes (#49)

--- lib/hackathon_manager.rb
+++ lib/hackathon_manager.rb
@@ -1,5 +1,15 @@
 require "hackathon_manager/engine"
 
 module HackathonManager
-  # Your code goes here...
+  def self.reload_config(app)
+    hackathon = app.config_for(:hackathon)
+
+    # Applications without a specified config.time_zone will parse
+    # this as a string instead of a DateTime
+    if hackathon['last_day_to_apply'].is_a?(String)
+      hackathon['last_day_to_apply'] = DateTime.parse(hackathon['last_day_to_apply'])
+    end
+
+    app.config.hackathon = hackathon
+  end
 end
