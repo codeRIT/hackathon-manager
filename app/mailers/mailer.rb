be52ccf9e30a220df6d0c40421eb053bae9208e4
Stuart Olivera <stuart@stuartolivera.com>
2018-01-08 01:39:36 -0500

    Don't throw error when unable to lookup object in mail job
    Instead, let the presence/blank conditions quit the job early.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -5,32 +5,32 @@ class Mailer < ApplicationMailer
   default from: Rails.configuration.hackathon['email_from']
 
   def application_confirmation_email(questionnaire_id)
-    @questionnaire = Questionnaire.find(questionnaire_id)
+    @questionnaire = Questionnaire.find_by_id(questionnaire_id)
     return unless @questionnaire.present? && @questionnaire.user.present?
     mail_questionnaire("Application Received")
   end
 
   def rsvp_confirmation_email(questionnaire_id)
-    @questionnaire = Questionnaire.find(questionnaire_id)
+    @questionnaire = Questionnaire.find_by_id(questionnaire_id)
     return unless @questionnaire.present? && @questionnaire.user.present?
     mail_questionnaire("RSVP Confirmation")
   end
 
   def accepted_email(questionnaire_id)
-    @questionnaire = Questionnaire.find(questionnaire_id)
+    @questionnaire = Questionnaire.find_by_id(questionnaire_id)
     return unless @questionnaire.present? && @questionnaire.user.present?
     mail_questionnaire("You've been accepted!")
   end
 
   def denied_email(questionnaire_id)
-    @questionnaire = Questionnaire.find(questionnaire_id)
+    @questionnaire = Questionnaire.find_by_id(questionnaire_id)
     return unless @questionnaire.present? && @questionnaire.user.present?
     mail_questionnaire("Your application status")
   end
 
   def bulk_message_email(message_id, user_id)
-    @message = Message.find(message_id)
-    @user    = User.find(user_id)
+    @message = Message.find_by_id(message_id)
+    @user    = User.find_by_id(user_id)
     return if @user.blank? || @message.blank?
     mail(
       to: pretty_email(@user.full_name, @user.email),
@@ -39,7 +39,7 @@ class Mailer < ApplicationMailer
   end
 
   def incomplete_reminder_email(user_id)
-    @user = User.find(user_id)
+    @user = User.find_by_id(user_id)
     return if @user.blank? || @user.questionnaire || Time.now.to_date > Rails.configuration.hackathon['last_day_to_apply']
     mail(
       to: @user.email,
@@ -48,21 +48,21 @@ class Mailer < ApplicationMailer
   end
 
   def bus_captain_confirmation_email(bus_list_id, user_id)
-    @user = User.find(user_id)
+    @user = User.find_by_id(user_id)
     @questionnaire = @user.questionnaire
-    @bus_list = BusList.find(bus_list_id)
+    @bus_list = BusList.find_by_id(bus_list_id)
     return if @user.blank? || @user.questionnaire.blank? || !@user.questionnaire.is_bus_captain? || @bus_list.blank?
     mail_questionnaire("You're a bus captain!")
   end
 
   def slack_invite_email(questionnaire_id)
-    @questionnaire = Questionnaire.find(questionnaire_id)
+    @questionnaire = Questionnaire.find_by_id(questionnaire_id)
     return if @questionnaire.blank?
     mail_questionnaire("Slack Invite!")
   end
 
   def bus_list_update_email(questionnaire_id)
-    @questionnaire = Questionnaire.find(questionnaire_id)
+    @questionnaire = Questionnaire.find_by_id(questionnaire_id)
     @bus_list = @questionnaire.bus_list
     return if @questionnaire.blank? || @questionnaire.user.blank? || @bus_list.blank?
     mail_questionnaire("Bus Update")
