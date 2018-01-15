bdb780f4342312bbc69ee04d68467816624a8f85
Stuart Olivera <stuart@stuartolivera.com>
2018-01-14 21:44:44 -0500

    Consolidate email events

--- app/assets/javascripts/hackathon_manager/manage/lib/emailEvents.js
+++ app/assets/javascripts/hackathon_manager/manage/lib/emailEvents.js
@@ -12,11 +12,27 @@ $(document).ready(function() {
           return;
         }
 
-        var $new_html = $('<ul>');
-        $(json).each(function(_, event) {
-          $new_html.append('<li><p>' + event.subject + ' : <strong>' + event.type + '</strong><br><em>' + event.timestamp + '</em></p></li>');
-        });
-        $(events_container).html($new_html);
+        const reducer = function(accumulator, event) {
+          const key = event.message_id;
+          if (key in accumulator === false) {
+            accumulator[key] = [];
+          }
+          accumulator[key].push(event);
+          return accumulator;
+        };
+        const groupedEvents = json.reduce(reducer, {});
+
+        const innerHtml = Object.keys(groupedEvents).map(function(key) {
+          const group = groupedEvents[key];
+          const groupHtml = group.map(function(event) {
+            const timestamp = new Date(event.timestamp);
+            return '<br /><small>' + event.type + ' at ' + timestamp.toLocaleString() + '</small>';
+          }).join('');
+          return '<li><p><strong>' + group[0].subject + '</strong>' + groupHtml + '</li>';
+        }).join('');
+        const newHtml = '<ul>' + innerHtml + '</ul>';
+
+        $(events_container).html(newHtml);
       })
       .fail(function() {
         $(events_container).html('<em>An error ocurred. Please try again later.</em>');
