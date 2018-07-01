03daafe39c1df74346f735467c569d4d428986d4
Stuart Olivera <stuart@stuartolivera.com>
2018-07-01 12:34:20 -0400

    Fix email support in stats + refactor controller

--- app/controllers/manage/stats_controller.rb
+++ app/controllers/manage/stats_controller.rb
@@ -4,31 +4,51 @@ class Manage::StatsController < Manage::ApplicationController
 
   def dietary_special_needs
     data = Rails.cache.fetch(cache_key_for_questionnaires("dietary_special_needs")) do
-      attributes = [:first_name,
-                    :last_name,
-                    :email,
-                    :phone,
-                    :checked_in_at,
-                    :dietary_restrictions,
-                    :special_needs]
-      select_attributes = Array.new(attributes) << :user_id
+      select_attributes = [
+        :first_name,
+        :last_name,
+        :phone,
+        :checked_in_at,
+        :dietary_restrictions,
+        :special_needs,
+        :user_id
+      ]
+      json_attributes = [
+        :first_name,
+        :last_name,
+        :email,
+        :phone,
+        :checked_in_at,
+        :dietary_restrictions,
+        :special_needs
+      ]
       data = Questionnaire.where("dietary_restrictions != '' AND acc_status = 'rsvp_confirmed' OR special_needs != '' AND acc_status = 'rsvp_confirmed'").select(select_attributes)
-      to_json_array(data, attributes)
+      to_json_array(data, json_attributes)
     end
     render json: { data: data }
   end
 
   def sponsor_info
     data = Rails.cache.fetch(cache_key_for_questionnaires("sponsor_info")) do
-      attributes = [:first_name,
-                    :last_name,
-                    :email,
-                    :vcs_url,
-                    :portfolio_url]
-      select_attributes = Array.new(attributes) << [:user_id, :school_id, :resume_file_name]
-      json_attribute = Array.new(attributes) << :school_name
+      select_attributes = [
+        :first_name,
+        :last_name,
+        :vcs_url,
+        :portfolio_url,
+        :user_id,
+        :school_id,
+        :resume_file_name
+      ]
+      json_attributes = [
+        :first_name,
+        :last_name,
+        :email,
+        :school_name,
+        :vcs_url,
+        :portfolio_url
+      ]
       data = Questionnaire.where("can_share_info = '1' AND checked_in_at != 0").select(select_attributes)
-      json = to_json_array(data, json_attribute)
+      json = to_json_array(data, json_attributes)
       json.map.with_index { |item, index| item.insert(5, data[index].resume? ? data[index].resume.url : '') }
     end
     render json: { data: data }
@@ -36,15 +56,25 @@ class Manage::StatsController < Manage::ApplicationController
 
   def alt_travel
     data = Rails.cache.fetch(cache_key_for_questionnaires("alt_travel")) do
-      attributes = [:id,
-                    :first_name,
-                    :last_name,
-                    :email,
-                    :travel_location,
-                    :acc_status]
-      select_attributes = Array.new(attributes) << [:user_id, :school_id]
+      select_attributes = [
+        :id,
+        :first_name,
+        :last_name,
+        :travel_location,
+        :acc_status,
+        :user_id,
+        :school_id
+      ]
+      json_attributes = [
+        :id,
+        :first_name,
+        :last_name,
+        :email,
+        :travel_location,
+        :acc_status
+      ]
       data = Questionnaire.where("travel_not_from_school = '1'").select(select_attributes)
-      json = to_json_array(data, attributes)
+      json = to_json_array(data, json_attributes)
       json.each do |e|
         e[0] = view_context.link_to("View &raquo;".html_safe, manage_questionnaire_path(e[0]))
       end
@@ -54,16 +84,22 @@ class Manage::StatsController < Manage::ApplicationController
 
   def mlh_info
     data = Rails.cache.fetch(cache_key_for_questionnaires("mlh_info")) do
-      attributes = [
+      select_attributes = [
+        :first_name,
+        :last_name,
+        :phone,
+        :user_id,
+        :school_id
+      ]
+      json_attributes = [
         :first_name,
         :last_name,
         :email,
-        :phone
+        :phone,
+        :school_name
       ]
-      select_attributes = Array.new(attributes) << [:user_id, :school_id]
-      json_attribute = Array.new(attributes) << :school_name
       data = Questionnaire.joins(:school).select(select_attributes)
-      to_json_array(data, json_attribute)
+      to_json_array(data, json_attributes)
     end
     render json: { data: data }
   end
