0663c9a718ab19e6d0b757d3b00f08a321ccd867
Stuart Olivera <stuart@stuartolivera.com>
2019-01-06 03:22:56 -0500

    Support older javascript syntax

--- app/assets/javascripts/hackathon_manager/manage/lib/simpleMde.js
+++ app/assets/javascripts/hackathon_manager/manage/lib/simpleMde.js
@@ -1,18 +1,18 @@
 function setupSimpleMde() {
-  $("[data-simple-mde]").each((i, element) => {
-    const options = {
+  $("[data-simple-mde]").each(function(i, element) {
+    var options = {
       element: element,
       forceSync: true
     };
     if ($(element).data("message-live-preview-base-src")) {
-      options["previewRender"] = plainText => {
-        const iframe = document.createElement("iframe");
+      options["previewRender"] = function(plainText) {
+        var iframe = document.createElement("iframe");
         var baseSrc = $(element).data("message-live-preview-base-src");
         var newSrc = baseSrc + "?body=" + encodeURIComponent(plainText);
         iframe.className = "email-preview-in-simplemde";
         iframe.src = newSrc;
         return iframe.outerHTML;
-      };
+      }.bind(this);
     }
     new SimpleMDE(options);
   });
