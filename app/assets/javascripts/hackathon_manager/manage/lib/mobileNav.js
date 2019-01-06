bfec2a0142317cf1d3dd773eac658bbc456cd7f4
Stuart Olivera <stuart@stuartolivera.com>
2019-01-06 02:28:53 -0500

    Implement mobile nav
    Fixes #84

---
+++ app/assets/javascripts/hackathon_manager/manage/lib/mobileNav.js
@@ -0,0 +1,8 @@
+function toggleMobileNav() {
+  $(".sidebar").toggleClass("sidebar-mobile");
+  if ($(".sidebar").hasClass("sidebar-mobile")) {
+    $(".navbar-mobile-toggle").addClass("navbar-mobile-toggle--active");
+  } else {
+    $(".navbar-mobile-toggle").removeClass("navbar-mobile-toggle--active");
+  }
+}
