43a223771a3f2c4688692770ee82e0eeb78ebc1c
Stuart Olivera <stuart@stuartolivera.com>
2019-01-07 19:25:36 -0500

    Fix foreign key for old table in migration

--- db/migrate/20190107233210_create_trackable_events.rb
+++ db/migrate/20190107233210_create_trackable_events.rb
@@ -3,7 +3,7 @@ class CreateTrackableEvents < ActiveRecord::Migration[5.2]
     create_table :trackable_events do |t|
       t.string :band_id
       t.references :trackable_tag, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :user, type: :integer, foreign_key: true
 
       t.timestamps
     end
