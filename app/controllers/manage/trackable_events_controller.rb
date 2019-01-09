aa340a55578cf52f396ef39b8bfa559f96cb13a3
Stuart Olivera <stuart@stuartolivera.com>
2019-01-09 01:29:19 -0500

    Implement logic for trackable event controller + tests

--- app/controllers/manage/trackable_events_controller.rb
+++ app/controllers/manage/trackable_events_controller.rb
@@ -1,9 +1,10 @@
 class Manage::TrackableEventsController < Manage::ApplicationController
   before_action :set_trackable_event, only: [:show, :edit, :update, :destroy]
+  before_action :scope_limited_admin_access, only: [:edit, :update, :destroy]
 
   # GET /manage/trackable_events
   def index
-    @trackable_events = TrackableEvent.all
+    @trackable_events = current_user.admin_limited_access ? TrackableEvent.where(user: current_user) : TrackableEvent.all
   end
 
   # GET /manage/trackable_events/1
@@ -21,7 +22,7 @@ class Manage::TrackableEventsController < Manage::ApplicationController
 
   # POST /manage/trackable_events
   def create
-    @trackable_event = TrackableEvent.new(trackable_event_params)
+    @trackable_event = TrackableEvent.new(trackable_event_params.merge(user_id: current_user.id))
 
     if @trackable_event.save
       redirect_to manage_trackable_event_path(@trackable_event), notice: 'Trackable event was successfully created.'
@@ -53,6 +54,18 @@ class Manage::TrackableEventsController < Manage::ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def trackable_event_params
-      params.require(:trackable_event).permit(:band_id, :trackable_tag_id, :user_id)
+      params.require(:trackable_event).permit(:band_id, :trackable_tag_id)
+    end
+
+    # Permit limited-access admins (overrides Manage::ApplicationController#limit_admin_access)
+    def limit_admin_access
+    end
+
+    # If the admin is limited, scope changes only to those they created
+    def scope_limited_admin_access
+      return if !current_user.admin_limited_access || @trackable_event.blank? || @trackable_event.user.blank?
+      if @trackable_event.user != current_user
+        redirect_to manage_trackable_events_path, notice: 'You may not view events you did not create.'
+      end
     end
 end
---
+++ test/controllers/manage/trackable_events_controller_test.rb
@@ -0,0 +1,221 @@
+require 'test_helper'
+
+class Manage::TrackableEventsControllerTest < ActionController::TestCase
+  setup do
+    @trackable_event = create(:trackable_event)
+  end
+
+  unauth_conditions = {
+    'not authenticated' => false,
+    'authenticated as a user' => true
+  }
+
+  unauth_conditions.each do |condition_name, do_sign_in|
+    context "while #{condition_name}" do
+      setup do
+        if do_sign_in
+          @user = create(:user)
+          @request.env["devise.mapping"] = Devise.mappings[:admin]
+          sign_in @user
+          @trackable_event.update_attribute(:user, @user)
+        end
+      end
+
+      should "not get index" do
+        test_index_failure
+      end
+
+      should "not get new" do
+        test_new_failure
+      end
+
+      should "not create trackable_event" do
+        test_create_failure
+      end
+
+      should "not show trackable_event" do
+        test_show_failure
+      end
+
+      should "not get edit" do
+        test_edit_failure
+      end
+
+      should "not update trackable_event" do
+        test_update_failure
+      end
+
+      should "not destroy trackable_event" do
+        test_destroy_failure
+      end
+    end
+  end
+
+  context "while authenticated as a limited access admin" do
+    setup do
+      @user = create(:limited_access_admin)
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @user
+      @trackable_event.update_attribute(:user, @user)
+    end
+
+    should "get index" do
+      test_index_success
+    end
+
+    should "get new" do
+      test_new_success
+    end
+
+    should "create trackable_event" do
+      test_create_success
+    end
+
+    should "show trackable_event" do
+      test_show_success
+    end
+
+    should "get edit" do
+      test_edit_success
+    end
+
+    should "update trackable_event" do
+      test_update_success
+    end
+
+    should "not update trackable_event that doesn't belong to user" do
+      @trackable_event.update_attribute(:user, create(:user))
+      test_update_failure
+    end
+
+    should "destroy trackable_event" do
+      test_destroy_success
+    end
+
+    should "not destroy trackable_event that doesn't belong to user" do
+      @trackable_event.update_attribute(:user, create(:user))
+      test_destroy_failure
+    end
+  end
+
+  context "while authenticated as an admin" do
+    setup do
+      @user = create(:admin)
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @user
+    end
+
+    should "get index" do
+      test_index_success
+    end
+
+    should "get new" do
+      test_new_success
+    end
+
+    should "create trackable_event" do
+      test_create_success
+    end
+
+    should "show trackable_event" do
+      test_show_success
+    end
+
+    should "get edit" do
+      test_edit_success
+    end
+
+    should "update trackable_event" do
+      test_update_success
+    end
+
+    should "destroy trackable_event" do
+      test_destroy_success
+    end
+  end
+
+  private
+
+  def test_index_success
+    get :index
+    assert_response :success
+  end
+
+  def test_index_failure
+    get :index
+    assert_response :redirect
+  end
+
+  def test_new_success
+    get :new
+    assert_response :success
+  end
+
+  def test_new_failure
+    get :new
+    assert_response :redirect
+  end
+
+  def test_create_success
+    assert_difference('TrackableEvent.count') do
+      post :create, params: { trackable_event: { band_id: @trackable_event.band_id, trackable_tag_id: @trackable_event.trackable_tag_id } }
+    end
+
+    assert_redirected_to manage_trackable_event_url(TrackableEvent.last)
+    assert_equal @user.id, TrackableEvent.last.user_id
+  end
+
+  def test_create_failure
+    assert_difference('TrackableEvent.count', 0) do
+      post :create, params: { trackable_event: { band_id: @trackable_event.band_id, trackable_tag_id: @trackable_event.trackable_tag_id } }
+    end
+
+    assert_response :redirect
+  end
+
+  def test_show_success
+    get :show, params: { id: @trackable_event.id }
+    assert_response :success
+  end
+
+  def test_show_failure
+    get :show, params: { id: @trackable_event.id }
+    assert_response :redirect
+  end
+
+  def test_edit_success
+    get :edit, params: { id: @trackable_event.id }
+    assert_response :success
+  end
+
+  def test_edit_failure
+    get :edit, params: { id: @trackable_event.id }
+    assert_response :redirect
+  end
+
+  def test_update_success
+    patch :update, params: { id: @trackable_event.id, trackable_event: { band_id: @trackable_event.band_id, trackable_tag_id: @trackable_event.trackable_tag_id } }
+    assert_redirected_to manage_trackable_event_url(@trackable_event)
+  end
+
+  def test_update_failure
+    patch :update, params: { id: @trackable_event.id, trackable_event: { band_id: @trackable_event.band_id, trackable_tag_id: @trackable_event.trackable_tag_id } }
+    assert_response :redirect
+  end
+
+  def test_destroy_success
+    assert_difference('TrackableEvent.count', -1) do
+      delete :destroy, params: { id: @trackable_event.id }
+    end
+
+    assert_redirected_to manage_trackable_events_url
+  end
+
+  def test_destroy_failure
+    assert_difference('TrackableEvent.count', 0) do
+      delete :destroy, params: { id: @trackable_event.id }
+    end
+
+    assert_response :redirect
+  end
+end
