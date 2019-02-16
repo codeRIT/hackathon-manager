5788bc9a0e974da05f51cf362b9c53590a08a9c5
Stuart Olivera <stuart@stuartolivera.com>
2019-02-16 17:51:55 -0500

    Configure hackathon via web UI instead of hackathon.yml
    Fixes #14

--- app/controllers/manage/configs_controller.rb
+++ app/controllers/manage/configs_controller.rb
@@ -1,10 +1,39 @@
 class Manage::ConfigsController < Manage::ApplicationController
   before_action :limit_access_admin
+  before_action :get_config, only: [:edit, :update]
 
   respond_to :html, :json
 
-  def show
-    respond_with(Rails.configuration.hackathon)
+  def index
+    @config = HackathonConfig.get_all
+    respond_with(HackathonConfig.get_all)
+  end
+
+  def edit
+  end
+
+  def update
+    value = params[:hackathon_config][:value]
+    value = true if value == 'true'
+    value = false if value == 'false'
+    if @config.value != value
+      @config.value = value
+      @config.save
+      redirect_to manage_configs_path, notice: 'Config has updated.'
+    else
+      redirect_to manage_configs_path
+    end
+  end
+
+  private
+
+  def get_config
+    var = params[:id]
+    @config = HackathonConfig.find_by(var: var)
+    if @config.blank?
+      @config = HackathonConfig.new(var: var)
+      @config.value = HackathonConfig[var]
+    end
   end
 
   private
