33e40f0cb519f0b4cc5231036e291dcd8cebe082
Stuart Olivera <stuart@stuartolivera.com>
2019-04-17 11:44:39 -0400

    Restore protect_from_forgery in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   def after_sign_in_path_for(resource)
     stored_location = stored_location_for(resource)
     if stored_location
