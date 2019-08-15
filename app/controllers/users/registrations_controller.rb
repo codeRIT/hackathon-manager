cef56e56856d8e46921dbf1da8549ebd120dd952
dquach217 <dq6180@rit.edu>
2019-08-15 12:44:44 -0400

    fix: when registration is over it should redirect to the root path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,9 +3,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    if HackathonConfig['disable_account_registration']
+      flash[:failure] = 'Registration had ended'
+      redirect_to root_path
+    else
+      super
+    end
+  end
 
   # POST /resource
   # def create
