54f2033131a03b8152d603e49cf0ae7732e04887
dquach217 <dq6180@rit.edu>
2019-08-19 22:33:47 -0400

    fix linting issues

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -256,10 +256,10 @@ class Questionnaire < ApplicationRecord
     if saved_change_to_acc_status? && acc_status == "accepted"
       days_remaining = Date.parse(HackathonConfig["event_start_date"]) - @questionnaire.acc_status_date.to_date
       if days_remaining > 14
-        deliver_date = 7.days.from_now 
-      elif days_remaining > 10
+        deliver_date = 7.days.from_now
+      elsif days_remaining > 10
         deliver_date = 5.days.from_now
-      elif days_remaining > 3
+      elsif days_remaining > 3
         deliver_date = 2.days.from_now
       end
       Message.queue_for_trigger("user.rsvp_reminder", user_id).deliver_later(wait_until: deliver_date) if deliver_date.present?
