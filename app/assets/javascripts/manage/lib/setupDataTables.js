cd9116108cd1e48ce71e2babf5d3dc619a91a361
dquach217 <dq6180@rit.edu>
2019-08-21 13:56:47 -0400

    remove the format that my editor applied

--- app/assets/javascripts/manage/lib/setupDataTables.js
+++ app/assets/javascripts/manage/lib/setupDataTables.js
@@ -4,149 +4,57 @@
  * See app/views/manage/application/_questionnaire_datatable.html.haml for an example.
  */
 
-var setupDataTables = function () {
+var setupDataTables = function() {
   $('.datatable.checkins').DataTable({
     order: [1, 'asc'],
-    columns: [{
-        orderable: true,
-        data: 'first_name',
-        visible: false
-      },
-      {
-        orderable: true,
-        data: 'last_name',
-        visible: false
-      },
-      {
-        orderable: false,
-        data: 'about'
-      },
-      {
-        orderable: true,
-        data: 'checked_in'
-      },
-      {
-        orderable: false,
-        data: 'actions'
-      },
+    columns: [
+      { orderable: true, data: 'first_name', visible: false },
+      { orderable: true, data: 'last_name', visible: false },
+      { orderable: false, data: 'about' },
+      { orderable: true, data: 'checked_in' },
+      { orderable: false, data: 'actions' },
     ],
   });
 
   $('.datatable.users').DataTable({
     order: [1, 'asc'],
-    columns: [{
-        orderable: true,
-        data: 'id',
-        visible: false
-      },
-      {
-        orderable: true,
-        data: 'email'
-      },
-      {
-        orderable: true,
-        data: 'role'
-      },
-      {
-        orderable: true,
-        data: 'active'
-      },
-      {
-        orderable: true,
-        data: 'receive_weekly_report'
-      },
-      {
-        orderable: true,
-        data: 'created_at'
-      },
-      {
-        orderable: true,
-        data: 'current_sign_in_at'
-      },
-      {
-        orderable: true,
-        data: 'last_sign_in_at'
-      },
-      {
-        orderable: true,
-        data: 'current_sign_in_ip'
-      },
-      {
-        orderable: true,
-        data: 'last_sign_in_ip'
-      },
-      {
-        orderable: true,
-        data: 'sign_in_count'
-      },
+    columns: [
+      { orderable: true, data: 'id', visible: false },
+      { orderable: true, data: 'email' },
+      { orderable: true, data: 'role' },
+      { orderable: true, data: 'active' },
+      { orderable: true, data: 'receive_weekly_report' },
+      { orderable: true, data: 'created_at' },
+      { orderable: true, data: 'current_sign_in_at' },
+      { orderable: true, data: 'last_sign_in_at' },
+      { orderable: true, data: 'current_sign_in_ip' },
+      { orderable: true, data: 'last_sign_in_ip' },
+      { orderable: true, data: 'sign_in_count' },
     ],
   });
 
   $('.datatable.bulk-messages').DataTable({
     order: [4, 'desc'],
-    columns: [{
-        orderable: true,
-        data: 'id',
-        visible: false
-      },
-      {
-        orderable: true,
-        data: 'name'
-      },
-      {
-        orderable: true,
-        data: 'subject'
-      },
-      {
-        orderable: false,
-        data: 'status'
-      },
-      {
-        orderable: true,
-        data: 'created_at'
-      },
-      {
-        orderable: true,
-        data: 'updated_at',
-        visible: false
-      },
-      {
-        orderable: true,
-        data: 'delivered_at'
-      },
+    columns: [
+      { orderable: true, data: 'id', visible: false },
+      { orderable: true, data: 'name' },
+      { orderable: true, data: 'subject' },
+      { orderable: false, data: 'status' },
+      { orderable: true, data: 'created_at' },
+      { orderable: true, data: 'updated_at', visible: false },
+      { orderable: true, data: 'delivered_at' },
     ],
   });
 
   $('.datatable.schools').DataTable({
-    order: [
-      [5, 'desc'],
-      [4, 'desc']
-    ],
-    columns: [{
-        orderable: true,
-        data: 'id',
-        visible: false
-      },
-      {
-        orderable: true,
-        data: 'name'
-      },
-      {
-        orderable: true,
-        data: 'city'
-      },
-      {
-        orderable: true,
-        data: 'state'
-      },
-      {
-        orderable: true,
-        data: 'questionnaire_count'
-      },
-      {
-        orderable: true,
-        data: 'home_school'
-      },
+    order: [[5, 'desc'], [4, 'desc']],
+    columns: [
+      { orderable: true, data: 'id', visible: false },
+      { orderable: true, data: 'name' },
+      { orderable: true, data: 'city' },
+      { orderable: true, data: 'state' },
+      { orderable: true, data: 'questionnaire_count' },
+      { orderable: true, data: 'home_school' },
     ],
   });
 
