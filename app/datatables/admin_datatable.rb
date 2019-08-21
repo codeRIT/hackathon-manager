ecaec4543060083af75a5ee13edb7c9e9b50158c
dquach217 <dq6180@rit.edu>
2019-08-21 13:40:12 -0400

    fix getting the data from the javascript side and changing the naming convention of table

--- app/datatables/admin_datatable.rb
+++ app/datatables/admin_datatable.rb
@@ -9,15 +9,19 @@ class AdminDatatable < ApplicationDatatable
       active: { source: "User.is_active", searchable: false },
       receive_weekly_report: { source: "User.receive_weekly_report", searchable: false },
       created_at: { source: "User.created_at", searchable: false },
-      current_sign_in_at: { source: "User.current_sign_in_at", searchable: false },
-      last_sign_in_at: { source: "User.last_sign_in_at", searchable: false },
-      current_sign_in_ip: { source: "User.current_sign_in_ip", searchable: false },
-      last_sign_in_ip: { source: "User.last_sign_in_ip", searchable: false },
-      sign_in_count: { source: "User.sign_in_count", searchable: false },
+      current_sign_in_at: { source: "User.current_sign_in_at" },
+      last_sign_in_at: { source: "User.last_sign_in_at" },
+      current_sign_in_ip: { source: "User.current_sign_in_ip" },
+      last_sign_in_ip: { source: "User.last_sign_in_ip" },
+      sign_in_count: { source: "User.sign_in_count" },
     }
   end
 
   private
+
+  def ip(record)
+    return ( record == "::1" ) ? "127.0.0.1" : record
+  end
 
   def data
     records.map do |record|
@@ -30,8 +34,8 @@ class AdminDatatable < ApplicationDatatable
         created_at: display_datetime(record.created_at),
         current_sign_in_at: display_datetime(record.current_sign_in_at),
         last_sign_in_at: display_datetime(record.last_sign_in_at),
-        current_sign_in_ip: record.current_sign_in_ip,
-        last_sign_in_ip: record.last_sign_in_ip,
+        current_sign_in_ip: ip(record.current_sign_in_ip),
+        last_sign_in_ip: ip(record.last_sign_in_ip),
         sign_in_count: record.sign_in_count,
       }
     end
