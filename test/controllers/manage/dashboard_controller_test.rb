34cd8490e731c3113a7a686966b6b897cfe21688
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2020-05-23 18:51:00 -0400

    fix(dashboard): Switches map to use different geocoder, reenables map

--- test/controllers/manage/dashboard_controller_test.rb
+++ test/controllers/manage/dashboard_controller_test.rb
@@ -45,8 +45,8 @@ class Manage::DashboardControllerTest < ActionController::TestCase
         FactoryBot.create_list(:questionnaire, 1, school_id: school2.id, acc_status: status)
       end
 
-      stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Rochester%20NY&sensor=true").
-        to_return(status: 200, body: '{"results":[{"geometry":{"location":{"lat": 100, "lng": 100}}}]}', headers: {'Content-Type' => 'application/json; charset=UTF-8'})
+      stub_request(:get, "https://geocoding.geo.census.gov/geocoder/locations/address?street=1+Lomb+Memorial+Dr&city=Rochester&state=New+York&benchmark=Public_AR_Current&format=json").
+        to_return(status: 200, body: '{"result":{"addressMatches":[{"coordinates":{"x": -77.67469, "y": 43.09212}}]}}', headers: {'Content-Type' => 'application/json; charset=UTF-8'})
       stub_request(:get, "https://geo.fcc.gov/api/census/area?format=json&lat=100&lon=100").
         to_return(status: 200, body: '{"results":[{"country_fips":1234}]}', headers: {'Content-Type' => 'application/json; charset=UTF-8'})
 
