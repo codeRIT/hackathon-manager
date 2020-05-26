5032247875e96ef220657ce6f4dacee43e766ee3
Peter Kos <pkos91@icloud.com>
2020-05-26 19:14:15 -0400

    refactor: Make HoundBot a little happier

--- db/migrate/20200525234832_move_user_info_from_questionnaire_to_user_model.rb
+++ db/migrate/20200525234832_move_user_info_from_questionnaire_to_user_model.rb
@@ -1,11 +1,9 @@
 class MoveUserInfoFromQuestionnaireToUserModel < ActiveRecord::Migration[5.2]
   def change
-  	
-  	remove_column :questionnaires, :first_name, :string
-  	remove_column :questionnaires, :last_name, :string
+    remove_column :questionnaires, :first_name, :string
+    remove_column :questionnaires, :last_name, :string
 
-  	add_column :users, :first_name, :string
-  	add_column :users, :last_name, :string
-
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
