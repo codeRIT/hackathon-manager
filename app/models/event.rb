503ed5be008ccc8b53b2e508de22e499c1fc4abd
Jeremy Rudman <jeremyrudman@gmail.com>
2020-06-13 08:14:25 -0400

    feat(events):page to view and make upcoming events
    created a page to view, make, and delete events. These events contain a
    title, description, location, owner(s), if it is a public and/or all day
    event and when it starts and stops.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,4 @@
 class Event < ApplicationRecord
   validates_presence_of :title, :owner ,:start, :end
-
-  # false.blank? is true, so you need a different validations then validates_presence_of for boolean data
-  validates :allDay, inclusion: { in: [true, false] }
-  validates :allDay, exclusion: { in: [nil] }
-  validates :public, inclusion: { in: [true, false] }
-  validates :public, exclusion: { in: [nil] }
-
+  
 end
