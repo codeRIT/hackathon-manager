0e2f503b7e713438362092802199f81ac5a7537f
Jeremy Rudman <jeremyrudman@gmail.com>
2020-06-13 07:05:27 -0400

    feat(events):page to view and make upcoming events
    created a page to view, make, and delete events. These events contain a
    title, description, location, owner(s), if it is a public and/or all day
    event and when it starts and stops.

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -7,9 +7,5 @@ class EventTest < ActiveSupport::TestCase
   should validate_presence_of :start
   should validate_presence_of :end
 
-  should validates :allDay, inclusion: { in: [true, false] }
-  should validates :allDay, exclusion: { in: [nil] }
-  should validates :public, inclusion: { in: [true, false] }
-  should validates :public, exclusion: { in: [nil] }
 
 end
