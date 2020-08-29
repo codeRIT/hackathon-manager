900a0a8eed29d681738f1735e07cd29a191f8483
Peter Kos <pkos91@icloud.com>
2020-08-29 18:51:20 -0400

    Fix bus captain bug, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_234832) do
+ActiveRecord::Schema.define(version: 2020_05_30_172450) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
