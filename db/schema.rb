8b4752839da860c385dd4b276707f85339d26143
Jeremy Rudman <jeremyrudman@gmail.com>
2020-09-05 15:18:47 -0400

    fix(events): refactored to work with latest 2.0
    altered new manage sidebar to include events and fixed bug in event
    model testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_172450) do
+ActiveRecord::Schema.define(version: 2020_08_06_002425) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
   should validate_presence_of :title
-  should validate_presence_of :owner
   should validate_presence_of :start
   should validate_presence_of :end
 end
