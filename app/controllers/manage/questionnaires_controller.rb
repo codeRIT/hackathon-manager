d1c3021a3a407284ae57f3a098ecd23ad93679bf
Peter Kos <pkos91@icloud.com>
2020-09-19 22:47:10 -0400

    On questnnre destroy, email admins if bus captain

--- app/controllers/manage/questionnaires_controller.rb
+++ app/controllers/manage/questionnaires_controller.rb
@@ -96,6 +96,13 @@ class Manage::QuestionnairesController < Manage::ApplicationController
   end
 
   def destroy
+    if @questionnaire.is_bus_captain
+      admins = User.where(role: :admin)
+      admins.each do |user|
+        AdminMailer.bus_captain_left(@questionnaire.bus_list_id, @questionnaire.user_id, user.id).deliver_later
+      end
+    end
+
     @questionnaire.destroy
     respond_with(:manage, @questionnaire)
   end
