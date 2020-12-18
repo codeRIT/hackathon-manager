16cc0c0009365bf878905384c1393a9e88df9ba4
Chris Baudouin, Jr <cjb5326@rit.edu>
2020-12-17 20:49:22 -0500

    fix: Fixes broken migrations

--- db/migrate/20201218010133_convert_agreements_to_text.rb
+++ db/migrate/20201218010133_convert_agreements_to_text.rb
@@ -1,6 +1,11 @@
 class ConvertAgreementsToText < ActiveRecord::Migration[5.2]
-  def change
-    change_column :agreements, :agreement_url, :text
+  def self.up
+    change_column :agreements, :agreement, :text
     rename_column :agreements, :agreement_url, :agreement
+  end
+
+  def self.down
+    rename_column :agreements, :agreement, :agreement_url
+    change_column :agreements, :agreement, :sring
   end
 end
