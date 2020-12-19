f80fcef5dab5e6b3961d3430da88abcf4808ba75
Chris Baudouin, Jr <cjb5326@rit.edu>
2020-12-19 00:09:44 -0500

    master <- develop (2.1.2) (#472) (#478)
    * fix(questionnaire): missing information now in correct place (#443)
    
    * fix(questionnair): missing information now in correct place
    
    it now displays the correct message "Please read & accept" instead of
    missing information and the notification is now in the correct place
    
    * Update app/assets/javascripts/validate.js
    
    * Revert "Update app/assets/javascripts/validate.js"
    
    This reverts commit bc54783f94d7a249ae878ad4ead6a9fb2173fee7.
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>
    
    * fix: Fixes mobile agreements layout bug
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * feat: Removes semantic-release (#446)
    
    * refactor: Moves CI to develop branch (#449)
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>
    
    * build: Merges 2.1.2 into develop
    
    * fix(hakiri): corrected unescaped model attribute
    
    * fix(hakiri): added html_safe to show proper output
    
    Co-authored-by: Jeremy Rudman <jeremyrudman@gmail.com>
    Co-authored-by: JeremyRudman <38338616+JeremyRudman@users.noreply.github.com>
    
    * fix: Shows questionnaires_closed_message on registration
    
    * feat: Allows agreements to be fully customizable (#465)
    
    * feat: Allows agreements to be fully customizable
    
    * fix: Fixes broken migrations
    
    * fix: Migrations misname issue
    
    * feat: Forces agreement links to open in new tab
    
    * Agreement validation detection fixed
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * refactor: Removes old input hint
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>
    
    * fix(hakiri): fixed un-escaped regex for vcs link (#467)
    
    * fix(hakiri): corrected unescaped model attribute
    
    * fix(hakiri): added html_safe to show proper output
    
    * fix(hakiri): added \A \z to regex
    
    * fix(questionnaire): fixed hakiri error with vcs link regex
    
    * fix(questionnaire): fixed houndci commplaint
    
    Co-authored-by: Chris Baudouin, Jr <cjb5326@rit.edu>
    
    * fix(hakiri): fix hakiri error with user input in html_safe (#475)
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>
    
    Co-authored-by: JeremyRudman <38338616+JeremyRudman@users.noreply.github.com>
    Co-authored-by: Chris Baudouin, Jr <cjb5326@rit.edu>
    Co-authored-by: Jeremy Rudman <jeremyrudman@gmail.com>
    
    Co-authored-by: Peter Kos <pkos91@icloud.com>
    Co-authored-by: JeremyRudman <38338616+JeremyRudman@users.noreply.github.com>
    Co-authored-by: Jeremy Rudman <jeremyrudman@gmail.com>

--- test/factories/agreement.rb
+++ test/factories/agreement.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :agreement do
     name { "HackFoo Agreement" }
-    agreement_url { "https://www.foo.com" }
+    agreement { "Please read and accept https://www.foo.com" }
   end
 end
