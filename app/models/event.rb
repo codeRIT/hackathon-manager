d61c80f32b799916d840d77e72d8110c7eccb626
Jeremy Rudman <jeremyrudman@gmail.com>
2021-01-23 04:20:12 -0500

    fix houndci complaint

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,8 @@ class Event < ApplicationRecord
   validate :finish_before_start
 
   def finish_before_start
-    unless finish == nil && finish > start
+    return unless !finish.nil?
+    unless finish > start
       errors.add(:finish, 'time must be after start time')
     end
   end
