dd390b35aea74f2f44183103bab1d4ebd7b2629e
Peter Kos <pkos91@icloud.com>
2021-02-03 00:04:34 -0500

    Resource path does not support regex; even tonly

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '^((?!manage).)*$', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '*/events.json', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
