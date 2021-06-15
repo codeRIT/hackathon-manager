cbffe53c2821978fd872613490e32229730fa0f7
Chris Baudouin, Jr <cjb5326@rit.edu>
2021-06-14 21:51:13 -0400

    fix: Fixes long code line

--- test/controllers/questionnaires_controller_test.rb
+++ test/controllers/questionnaires_controller_test.rb
@@ -122,7 +122,20 @@ class QuestionnairesControllerTest < ActionController::TestCase
 
       should "not allow questionnaire to be created" do
         assert_difference('Questionnaire.count', 0) do
-          post :create, params: { questionnaire: { experience: @questionnaire.experience, interest: @questionnaire.interest, country: @questionnaire.country, phone: @questionnaire.phone, level_of_study: @questionnaire.level_of_study, date_of_birth: @questionnaire.date_of_birth, shirt_size: @questionnaire.shirt_size, school_id: @school.id, major: @questionnaire.major, gender: @questionnaire.gender, why_attend: @questionnaire.why_attend, graduation_year: @questionnaire.graduation_year, race_ethnicity: @questionnaire.race_ethnicity, agreement_ids: @questionnaire.agreements.map(&:id) } }
+          post :create, params: { questionnaire: { experience: @questionnaire.experience, 
+                                                   interest: @questionnaire.interest, 
+                                                   country: @questionnaire.country, 
+                                                   phone: @questionnaire.phone, 
+                                                   level_of_study: @questionnaire.level_of_study, 
+                                                   date_of_birth: @questionnaire.date_of_birth, 
+                                                   shirt_size: @questionnaire.shirt_size, 
+                                                   school_id: @school.id, 
+                                                   major: @questionnaire.major, 
+                                                   gender: @questionnaire.gender, 
+                                                   why_attend: @questionnaire.why_attend, 
+                                                   graduation_year: @questionnaire.graduation_year, 
+                                                   race_ethnicity: @questionnaire.race_ethnicity, 
+                                                   agreement_ids: @questionnaire.agreements.map(&:id) } }
         end
 
         assert_response :not_acceptable
