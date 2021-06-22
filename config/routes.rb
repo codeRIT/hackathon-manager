9713768342addacba151cb252083e7d8ba109112
Chris Baudouin, Jr <cjb5326@rit.edu>
2021-06-21 21:42:54 -0400

    refactor(Events): Converts public Events to API only (#686)
    Co-authored-by: Peter Kos <pkos91@icloud.com>

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
     patch :boarded_bus, on: :collection
   end
 
-  resource :events, only: :show, constraints: ->(req) { req.format == :json }
+  resources :events, only: [:index, :show]
 
   namespace :manage do
     authenticate :user, ->(u) { u.director? } do
