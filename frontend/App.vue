294a3a0bbc207f6224d6549b8ecc880039389c45
Claire Gallup <claire.j.gallup@gmail.com>
2021-07-05 01:11:59 -0500

    Fine-tunnig profile page

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>Hello {{ name }}!</div>
-
-  <router-link to="/">Go to Home</router-link>
-  <router-link to="/about">Go to About</router-link>
-
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script>
