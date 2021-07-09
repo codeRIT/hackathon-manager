bd9fe6b7737240413fe88b6738ef0da337621684
Claire Gallup <claire.j.gallup@gmail.com>
2021-07-09 12:53:32 -0500

    Added header to questionnaire detail view

--- frontend/routes/manage/Questionnaire.vue
+++ frontend/routes/manage/Questionnaire.vue
@@ -1,5 +1,30 @@
 <template>
 <div class="container">
-    <h1>John Smith (ID #{{ $route.params.id }})</h1>
+    <div class="header">
+        <h1>John Smith (ID #{{ $route.params.id }})</h1>
+        <a href="#">edit</a>
+    </div>
 </div>
 </template>
+
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    align-items: baseline;
+    margin-bottom: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: 0.125rem solid lightgray;
+
+    * {
+        padding-bottom: 0;
+    }
+
+    *:first-child {
+        margin-right: auto;
+    }
+
+    a {
+        font-size: 1.17em;  // default h3
+    }
+}
+</style>
