a31c9c98edb60d360a12ba1adbf06029d7747045
Chris Baudouin, Jr <cjb5326@rit.edu>
2021-08-02 21:35:02 -0400

    refactor: Converts Schools to API only

--- app/controllers/manage/schools_controller.rb
+++ app/controllers/manage/schools_controller.rb
@@ -1,77 +1,59 @@
 class Manage::SchoolsController < Manage::ApplicationController
-  before_action :find_school, only: [:show, :edit, :update, :destroy, :merge, :perform_merge]
+  before_action :find_school, only: [:show, :update, :destroy, :perform_merge]
 
-  respond_to :html, :json
+  respond_to :json
 
   def index
-    respond_with(:manage, School.all)
-  end
-
-  def datatable
-    render json: SchoolDatatable.new(params, view_context: view_context)
+    @schools = School.all
   end
 
   def show
-    respond_with(:manage, @school)
-  end
-
-  def new
-    @school = ::School.new
-    respond_with(:manage, @school)
-  end
-
-  def edit
   end
 
   def create
     @school = ::School.new(school_params)
-    @school.save
-    respond_with(:manage, @school, location: manage_schools_path)
+    if @school.save
+      head :ok
+    else
+      head :unprocessable_entity
+    end
   end
 
   def update
-    @school.update_attributes(school_params)
-    respond_with(:manage, @school, location: manage_schools_path)
+    if @school.update_attributes(school_params)
+      head :ok
+    else
+      head :unprocessable_entity
+    end
   end
 
   def destroy
     @school.destroy
-    respond_with(:manage, @school, location: manage_schools_path)
-  end
-
-  def merge
+    head :ok
   end
 
   def perform_merge
     new_school_name = params[:school][:id]
     if new_school_name.blank?
-      flash[:alert] = "Error: Must include the new school to merge into!"
-      render :merge
+      head :bad_request
       return
     end
 
     new_school = School.where(name: new_school_name).first
     if new_school.blank?
-      flash[:alert] = "Error: School doesn't exist: #{new_school_name}"
-      render :merge
+      head :bad_request
       return
     end
 
-    Questionnaire.where(school_id: @school.id).each do |q|
+    old_school = @school.clone
+    @school.destroy
+    Questionnaire.where(school_id: old_school.id).each do |q|
       q.update_attribute(:school_id, new_school.id)
     end
 
-    SchoolNameDuplicate.create(name: @school.name, school_id: new_school.id)
+    SchoolNameDuplicate.create(name: old_school.name, school_id: new_school.id)
 
-    @school.reload
-
-    if @school.questionnaire_count < 1
-      @school.destroy
-    else
-      flash[:alert] = "*** Old school NOT deleted: #{@school.questionnaire_count} questionnaires still associated!\n"
-    end
-
-    redirect_to manage_school_path(new_school)
+    head :ok
   end
 
   private
