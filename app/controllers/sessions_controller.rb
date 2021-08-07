e4008bee8e7c094344dd060b8fdfe8dfd45be63b
Jeremy Rudman <jeremyrudman@gmail.com>
2021-08-07 12:07:41 -0700

    semi working authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < Devise::SessionsController
   def create
-    user = User.find_by_email(sign_in_params[:email])
+    user = User.find_by_email(params[:email])
 
-    if user && user.valid_password?(sign_in_params[:password])
+    if user && user.valid_password?(params[:password])
       @current_user = user
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
