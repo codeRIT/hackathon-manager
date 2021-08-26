69a2370a352974c27b7fcc1df4138c79df51e4d8
Claire Gallup <claire.j.gallup@gmail.com>
2021-08-25 23:07:05 -0400

    some amount of progress, idk, we're not using this branch anyway

--- frontend/index.js
+++ frontend/index.js
@@ -7,15 +7,15 @@ import App from "./App"
 import Home from "./routes/Home.vue"
 import About from "./routes/About.vue"
 import Profile from "./routes/Profile.vue"
-import Application from "./routes/Application.vue"
 import Dashboard from "./routes/manage/Dashboard.vue"
+import ComponentTest from "./routes/ComponentTest.vue"
 
 const routes = [
     { path: "/", component: Home },
     { path: "/about", component: About },
-    { path: "/application", component: Application},
     { path: "/profile", component: Profile },
-    { path: "/manage", component: Dashboard }
+    { path: "/manage", component: Dashboard },
+    { path: "/component-test", component: ComponentTest }
 ]
 
 const router = createRouter({
---
+++ frontend/routes/ComponentTest.vue
@@ -0,0 +1,7 @@
+<template>
+<button>test</button>
+</template>
+
+<style lang="scss">
+@import "../assets/scss/forms/button.scss";
+</style>
