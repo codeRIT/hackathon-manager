bb1da6c309788cb1853922fba9b52c52cbd84105
Peter Kos <pkos91@icloud.com>
2021-08-27 21:36:58 -0400

    Fix various issues with parcel

--- frontend/index.js
+++ frontend/index.js
@@ -3,17 +3,16 @@ import { createRouter, createWebHashHistory } from "vue-router"
 
 import App from "./App"
 
-
 import Home from "./routes/Home.vue"
 import About from "./routes/About.vue"
 import Profile from "./routes/Profile.vue"
-import Application from "./routes/Application.vue"
+// import Application from "./routes/Application.vue"
 import Dashboard from "./routes/manage/Dashboard.vue"
 
 const routes = [
     { path: "/", component: Home },
     { path: "/about", component: About },
-    { path: "/application", component: Application},
+    // { path: "/application", component: Application},
     { path: "/profile", component: Profile },
     { path: "/manage", component: Dashboard }
 ]
