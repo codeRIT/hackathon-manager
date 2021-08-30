b4193fa54aec259fce5dc9f0d60c6e90e141edc0
Peter Kos <pkos91@icloud.com>
2021-08-30 19:22:21 -0400

    Checkbox and radio progress

--- frontend/routes/Home.vue
+++ frontend/routes/Home.vue
@@ -17,22 +17,30 @@
 
     <br><br><br>
 
-    <input type="checkbox" id="first" class="checkbox" name="first" value="Banana">
-    <label for="first"> I have a banana</label><br>
+    <label class="checkbox-container">
+        <input type="checkbox">
+        <span class="checkbox"></span>
+    </label>
 
-    <input type="checkbox" id="second" class="checkbox" name="second" value="Orange">
-    <label for="second"> I have a orange</label><br>
+    <br><br><br>
 
-    <input type="checkbox" id="third" class="checkbox" name="third" value="Apple">
-    <label for="third"> I have a apple</label><br>
+    <label class="radio-container">
+        <input type="radio" name="asdf" value="123" checked>
+        <span class="checkbox radio"></span>
+    </label>
+
+    <br><br>
+    <label class="radio-container">
+        <input type="radio" name="asdf" value="144;">
+        <span class="checkbox radio"></span>
+    </label>
 
 
 
 </template>
 
 <script>
-    export default {
-        data() {
+    export default {        data() {
             return {
                 name: "Home",
             };
@@ -45,9 +53,11 @@
 // FIXME: https://github.com/vuejs/vue-loader/issues/1601
 $dark-color: #4D4D4D;
 $shadow-length: 4px;
+$shadow-length-control: $shadow-length / 2;
 $border-radius: 6px;
 $border-size: 2px;
 $orange: #F76901;
+$duration: 0.1s;
 
 .card {
     display: inline-block;
@@ -69,7 +79,7 @@ $orange: #F76901;
     border: $border-size solid $dark-color;
     border-radius: $border-radius;
     box-shadow: $shadow-length $shadow-length $dark-color;
-    transition: 0.1s all;
+    transition: $duration all;
 
     &:hover {
         @extend .button-hover;
@@ -83,16 +93,15 @@ $orange: #F76901;
 
 .button-hover {
     @extend .button;
-    box-shadow: 0 0 $dark-color;
-    transform: translate($shadow-length, $shadow-length);
-
+    background-color: $orange;
+    color: white;
+    // font-weight: bold;
 }
 
 .button-active {
     @extend .button-hover;
-    background-color: $orange;
-    color: white;
-    font-weight: bold;
+    box-shadow: 0 0 $dark-color;
+    transform: translate($shadow-length, $shadow-length);
 }
 
 .input-text {
@@ -112,13 +121,84 @@ $orange: #F76901;
 
 
 // TODO: Checkbox
+
+.checkbox-container {
+    display: block;
+    position: relative;
+    cursor: pointer;
+    user-select: none;
+
+    // Hide default checkbox
+    input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+
+    }
+
+    // Hover state
+    &:hover input ~ .checkbox {
+        // background-color: yellow;
+        background-color: $orange;
+    }
+
+    input:checked ~ .checkbox {
+        // background-color: darkgreen;
+        &::before {
+            content: "";
+            $size: 70%;
+            width: $size;
+            height: $size;
+            top: (100% - $size) / 2;
+            left: (100% - $size) / 2;
+            position: absolute;
+            background-color: $dark-color;
+            border-radius: $border-radius / 2;
+        }
+    }
+
+    input ~ .checkbox {
+        transition: $duration all;
+    }
+
+    &:active input ~ .checkbox {
+        box-shadow: 0 0 $dark-color;
+        transform: translate($shadow-length-control, $shadow-length-control);
+    }
+}
+
 .checkbox {
-    box-shadow: $shadow-length $shadow-length $dark-color;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    background-color: white;
+    box-shadow: $shadow-length-control $shadow-length-control $dark-color;
+    border: $border-size solid $dark-color;
     border-radius: $border-radius;
 
+    &:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+}
+
+.radio-container {
+    @extend .checkbox-container;
+
+    input:checked ~ .radio::before {
+        border-radius: 50%;
+    }
 
 }
 
+
+.radio {
+    border-radius: 50%;
+
+}
 
 
 
