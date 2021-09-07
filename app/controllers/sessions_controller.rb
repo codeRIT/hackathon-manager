2c06ed620968ca8c3de75565ec153d0d8c5aec5e
Jeremy Rudman <jeremyrudman@gmail.com>
2021-09-06 21:04:40 -0400

    remove sessions controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,2 @@
 class SessionsController < Devise::SessionsController
-  def create
-    user = User.find_by_email(params[:email])
-
-    if user && user.valid_password?(params[:password])
-      @current_user = user
-    else
-      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
-    end
-  end
 end
