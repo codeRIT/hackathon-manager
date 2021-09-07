e33d3f4350276987ec9b272f5bf1775e54e48edd
Jeremy Rudman <jeremyrudman@gmail.com>
2021-09-06 21:39:46 -0400

    JWT token now expires after 30 days instead of 60

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
 
   def generate_jwt
     JWT.encode({ id: id,
-                 exp: 60.days.from_now.to_i },
+                 exp: 30.days.from_now.to_i },
                Rails.application.secrets.secret_key_base)
   end
 
