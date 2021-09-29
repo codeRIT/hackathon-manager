a6a13d22a7b8c2953cfd8ad4d8582039e9f7e048
dquach217 <dq6180@rit.edu>
2021-09-28 21:48:25 -0400

    update questionnaires to using the current standards
    add a couple of new errors to the class for the sole purpose for questionnaires
    
    remove all view files that was used for the original hackathonmanager

--- lib/v3/error_response.rb
+++ lib/v3/error_response.rb
@@ -2,7 +2,8 @@ class ErrorResponse
   attr_accessor :error_identifier, :default_message
 
   ERRORS = [:busLists_sendUpdateEmail_NotesEmpty,
-            :login_merge_newSchoolNameMissing].freeze
+            :login_merge_newSchoolNameMissing,
+            :questionnaire_updateAccStatus_blankStatus].freeze
 
   def initialize(error_identifier, default_message = nil)
     @error_identifier = error_identifier
