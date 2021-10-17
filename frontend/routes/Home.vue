0739b934d2dcc582c8a15d1215830d640a771959
Claire Gallup <claire.j.gallup@gmail.com>
2021-10-17 00:31:26 -0400

    Fixed Home.vue template having multiple root elements

--- frontend/routes/Home.vue
+++ frontend/routes/Home.vue
@@ -1,24 +1,26 @@
 <template>
-    <h1>Component Test</h1>
-
-    <div class="test">
-        <Card content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua."></Card>
-
-        <Button content="Button"></Button>
-        <Button content="Button"></Button>
-        <Button content="Button"></Button>
-
-        <TextInput placeholder="Give password"></TextInput>
-
-        <Checkbox content="Hello" name="checkbox-test" value="test"></Checkbox>
-
-        <Radio content="Hello" name="radio-test" value="asdf"></Radio>
-        <Radio content="Hello" name="radio-test" value="1234"></Radio>
-    </div>
     <div>
-        <p>{{ $t("pages.home.component") }}</p>
-        <p>{{ $t("pages.home.hello") }}</p>
+        <h1>Component Test</h1>
+
+        <div class="test">
+            <Card content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua."></Card>
+
+            <Button content="Button"></Button>
+            <Button content="Button"></Button>
+            <Button content="Button"></Button>
+
+            <TextInput placeholder="Give password"></TextInput>
+
+            <Checkbox content="Hello" name="checkbox-test" value="test"></Checkbox>
+
+            <Radio content="Hello" name="radio-test" value="asdf"></Radio>
+            <Radio content="Hello" name="radio-test" value="1234"></Radio>
+        </div>
+        <div>
+            <p>{{ $t("pages.home.component") }}</p>
+            <p>{{ $t("pages.home.hello") }}</p>
+        </div>
     </div>
 </template>
 
