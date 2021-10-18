dd6a59a26eb23ad3f02ed85824574f493dc37bc5
Claire Gallup <colbygallup@gmail.com>
2021-10-18 19:17:31 -0400

    feat: Implemented Table component from style guide (#851)
    * Started working on Table component
    
    * Fixed a couple bugs with table borders and render
    
    * Added note about issue #731
    
    * Started working on pagination (will finish later)
    
    * Scaffolded filters
    
    * Improved table display on small screens
    
    * Fixed table drop shadow on mobile
    
    * Commented out unimplemented table features
    
    * Fixed missing newline

--- frontend/routes/Home.vue
+++ frontend/routes/Home.vue
@@ -1,24 +1,29 @@
 <template>
-    <h1>Component Test</h1>
-
-    <div class="test">
-        <Card content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua."></Card>
-
-        <Button content="Button"></Button>
-        <Button content="Button"></Button>
-        <Button content="Button"></Button>
-
-        <TextInput placeholder="Give password"></TextInput>
-
-        <Checkbox content="Hello" name="checkbox-test" value="test"></Checkbox>
-
-        <Radio content="Hello" name="radio-test" value="asdf"></Radio>
-        <Radio content="Hello" name="radio-test" value="1234"></Radio>
-    </div>
     <div>
-        <p>{{ $t("pages.home.component") }}</p>
-        <p>{{ $t("pages.home.hello") }}</p>
+        <h1>Component Test</h1>
+
+        <div class="test">
+            <Card content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua."></Card>
+
+            <Button content="Button"></Button>
+            <Button content="Button"></Button>
+            <Button content="Button"></Button>
+
+            <TextInput placeholder="Give password"></TextInput>
+
+            <Checkbox content="Hello" name="checkbox-test" value="test"></Checkbox>
+
+            <Radio content="Hello" name="radio-test" value="asdf"></Radio>
+            <Radio content="Hello" name="radio-test" value="1234"></Radio>
+
+            <Table :rows="tableRows" :showPagination="true" editLink="/test"></Table>
+        </div>
+
+        <div>
+            <p>{{ $t("pages.home.component") }}</p>
+            <p>{{ $t("pages.home.hello") }}</p>
+        </div>
     </div>
 </template>
 
@@ -28,6 +33,7 @@
     import TextInput from "../components/TextInput.vue";
     import Checkbox  from "../components/Checkbox.vue";
     import Radio     from "../components/Radio.vue";
+    import Table     from "../components/Table.vue";
 
     export default {
         components: {
@@ -35,11 +41,39 @@
             Button,
             Checkbox,
             Radio,
-            TextInput
+            TextInput,
+            Table
         },
         data() {
             return {
                 name: "Home",
+                tableRows: [
+                    {
+                        "Column 1": "lorem ipsum",
+                        "Column 2": "lorem ipsum",
+                        "Column 3": "lorem ipsum"
+                    },
+                    {
+                        "Column 1": "lorem ipsum",
+                        "Column 2": "lorem ipsum",
+                        "Column 3": "lorem ipsum"
+                    },
+                    {
+                        "Column 1": "lorem ipsum",
+                        "Column 2": "lorem ipsum",
+                        "Column 3": "lorem ipsum"
+                    },
+                    {
+                        "Column 1": "lorem ipsum",
+                        "Column 2": "lorem ipsum",
+                        "Column 3": "lorem ipsum"
+                    },
+                    {
+                        "Column 1": "lorem ipsum",
+                        "Column 2": "lorem ipsum",
+                        "Column 3": "lorem ipsum"
+                    }
+                ]
             }
         },
     };
