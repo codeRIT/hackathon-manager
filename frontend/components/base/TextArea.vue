2a1878e0d8ca46db4c12f9fdad330469d4aa6753
Claire Gallup <colbygallup@gmail.com>
2021-10-29 12:36:42 -0400

    feat: Added TextArea component (#861)
    * Added TextArea component
    
    * Set default cols to 20 to match browser default

---
+++ frontend/components/base/TextArea.vue
@@ -0,0 +1,32 @@
+<template>
+    <textarea class="input-textarea" :cols="cols" :placeholder="placeholder"></textarea>
+</template>
+
+<script>
+export default {
+    name: 'TextArea',
+    props: {
+        placeholder: String,
+        cols: {
+            type: Number,
+            default: 20
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.input-textarea {
+    border-radius: var(--border-radius);
+    border: var(--border-size) solid var(--dark-color);
+    display: block;
+    font-family: unset;
+    padding: 6px;
+    outline: none;
+    width: 100%;
+
+    &:focus {
+       border-color: var(--orange);
+    }
+}
+</style>
