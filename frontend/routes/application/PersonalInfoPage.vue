c7068fd79cfb4d10d306a2241fc7b3ecc060fae3
Claire Gallup <claire.j.gallup@gmail.com>
2021-11-15 12:57:33 -0500

    Moved personal info strings to locale file

--- frontend/routes/application/PersonalInfoPage.vue
+++ frontend/routes/application/PersonalInfoPage.vue
@@ -7,25 +7,41 @@
 
         <div class="row">
             <div class="col-6">
-                <TextInput id="date_of_birth" :label="$t('pages.application.personalInfo.dateOfBirth')" placeholder="01/01/2000"></TextInput>
-                <Dropdown id="country" label="Country">
+                <TextInput
+                    id="date_of_birth"
+                    :label="$t('pages.application.personalInfo.dateOfBirth.label')"
+                    :placeholder="$t('pages.application.personalInfo.dateOfBirth.placeholder')"
+                ></TextInput>
+                <Dropdown id="country" :label="$t('pages.application.personalInfo.country')">
                     <option>United States</option>
                     <option>Germany</option>
                 </Dropdown>
-                <Dropdown id="school" label="School">
+                <Dropdown id="school" :label="$t('pages.application.personalInfo.school')">
                     <option>Rochester Institute of Technology</option>
                     <option>University of Rochester</option>
                 </Dropdown>
-                <TextInput id="gender" label="Gender" placeholder="Non-binary"></TextInput>
+                <TextInput
+                    id="gender"
+                    :label="$t('pages.application.personalInfo.gender.label')"
+                    :placeholder="$t('pages.application.personalInfo.gender.placeholder')"
+                ></TextInput>
             </div>
 
             <div class="col-6">
-                <TextInput id="phone" label="Phone number" placeholder="(123) 456-7890"></TextInput>
-                <Dropdown id="race_ethnicity" label="Race / Ethnicity">
-                    <option>White / Caucasian</option>
-                    <option>Hispanic</option>
+                <TextInput
+                    id="phone"
+                    :label="$t('pages.application.personalInfo.phone.label')"
+                    :placeholder="$t('pages.application.personalInfo.phone.placeholder')"
+                ></TextInput>
+                <Dropdown id="race_ethnicity" :label="$t('pages.application.personalInfo.raceEthnicity.label')">
+                    <option value="White / Caucasian">{{ $t('pages.application.personalInfo.raceEthnicity.whiteCaucasian') }}</option>
+                    <option value="Hispanic">{{ $t('pages.application.personalInfo.raceEthnicity.hispanic') }}</option>
                 </Dropdown>
-                <TextInput id="major" label="Major" placeholder="New Media Design"></TextInput>
+                <TextInput
+                    id="major"
+                    :label="$t('pages.application.personalInfo.major.label')"
+                    :placeholder="$t('pages.application.personalInfo.major.placeholder')"
+                ></TextInput>
             </div>
         </div>
     </Card>
