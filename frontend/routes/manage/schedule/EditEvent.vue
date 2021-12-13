5fcb6c999c3c17353cc626f7c41836f24c72fe03
Claire Gallup <claire.j.gallup@gmail.com>
2021-12-13 00:39:52 -0500

    slightly more poggress

--- frontend/routes/manage/schedule/EditEvent.vue
+++ frontend/routes/manage/schedule/EditEvent.vue
@@ -1,33 +1,41 @@
 <template>
-    <div>
-        <h1>Edit Event</h1>
+    <div class="row">
+        <div class="col-12">
+            <h1>Edit Event</h1>
 
-        <div class="row">
-            <div class="col-12">
-                <TextInput
-                    v-model="title"
-                    :label="$t('pages.manage.schedule.edit.title.label')"
-                    :placeholder="$t('pages.manage.schedule.edit.title.placeholder')"
-                ></TextInput>
+            <Card>
+                <div class="row">
+                    <div class="col-12">
+                        <TextInput
+                            v-model="title"
+                            :label="$t('pages.manage.schedule.edit.title.label')"
+                            :placeholder="$t('pages.manage.schedule.edit.title.placeholder')"
+                        ></TextInput>
 
-                <TextInput
-                    v-model="description"
-                    :label="$t('pages.manage.schedule.edit.description.label')"
-                    :placeholder="$t('pages.manage.schedule.edit.description.placeholder')"
-                ></TextInput>
+                        <TextArea
+                            v-model="description"
+                            :label="$t('pages.manage.schedule.edit.description.label')"
+                            :placeholder="$t('pages.manage.schedule.edit.description.placeholder')"
+                        ></TextArea>
 
-                <!-- TODO: add start/end datetime pickers -->
-            </div>
+                        <!-- TODO: add start/end datetime pickers -->
+                    </div>
+                </div>
+            </Card>
         </div>
     </div>
 </template>
 
 <script>
+import Card      from "../../../components/base/Card.vue"
+import TextArea  from "../../../components/base/TextArea.vue"
 import TextInput from "../../../components/base/TextInput.vue"
 
 export default {
     name: 'EditEvent',
     components: {
+        Card,
+        TextArea,
         TextInput
     },
     data() {
