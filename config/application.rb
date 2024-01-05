ce14df4c8b965dee404671f62b564bd186f9ad27
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2024-01-05 12:46:56 -0500

    Updates Rails to 6.1.7.6

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 require "./lib/hackathon_manager"
 
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module HackathonManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
