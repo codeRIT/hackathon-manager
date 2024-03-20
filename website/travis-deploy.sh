84f206d18886518eef6a4ae6c36254e01e431a63
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2024-03-19 22:50:08 -0400

    Removes existing docs (#1030)

--- website/travis-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Deploying docs website to GitHub Pages..."
-
-# See https://docusaurus.io/docs/en/publishing#using-travis-ci
-git config --global user.name "${GH_NAME}"
-git config --global user.email "${GH_EMAIL}"
-echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
-cd website && yarn install && GIT_USER="${GH_NAME}" yarn run publish-gh-pages
